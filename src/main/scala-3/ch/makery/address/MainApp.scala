5cee15ef7af990ba8ef07288833776e0394ad2b4
Teh Kai Xin <rachaelteh323@gmail.com>
2025-07-09 22:04:09 +0800

    09072025
    1

--- src/main/scala-3/ch/makery/address/MainApp.scala
+++ src/main/scala-3/ch/makery/address/MainApp.scala
@@ -1,16 +1,37 @@
 package ch.makery.address
 
+import ch.makery.address.model.Person
+import ch.makery.address.view.PersonEditDialogController
 import javafx.fxml.FXMLLoader
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.Includes.*
 import javafx.scene as jfxs
+import scalafx.collections.ObservableBuffer
+import scalafx.stage.{Modality, Stage}
 
 object MainApp extends JFXApp3:
 
   //Window Root Pane
   var roots: Option[scalafx.scene.layout.BorderPane] = None
+  /**
+   * The data as an observable list of Persons.
+   */
+  val personData = new ObservableBuffer[Person]()
+
+  /**
+   * Constructor
+   */
+  personData += new Person("Hans", "Muster")
+  personData += new Person("Ruth", "Mueller")
+  personData += new Person("Heinz", "Kurz")
+  personData += new Person("Cornelia", "Meier")
+  personData += new Person("Werner", "Meyer")
+  personData += new Person("Lydia", "Kunz")
+  personData += new Person("Anna", "Best")
+  personData += new Person("Stefan", "Meier")
+  personData += new Person("Martin", "Mueller")
 
   override def start(): Unit =
     // transform path of RootLayout.fxml to URI for resource location.
@@ -37,3 +58,21 @@ object MainApp extends JFXApp3:
     loader.load()
     val roots = loader.getRoot[jfxs.layout.AnchorPane]
     this.roots.get.center = roots
+
+  def showPersonEditDialog(person: Person): Boolean =
+    val resource = getClass.getResource("view/PersonEditDialog.fxml")
+    val loader = new FXMLLoader(resource)
+    loader.load();
+    val roots2 = loader.getRoot[jfxs.Parent]
+    val control = loader.getController[PersonEditDialogController]
+
+    val dialog = new Stage():
+      initModality(Modality.ApplicationModal)
+      initOwner(stage)
+      scene = new Scene:
+        root = roots2
+
+    control.dialogStage = dialog
+    control.person = person
+    dialog.showAndWait()
+    control.okClicked
